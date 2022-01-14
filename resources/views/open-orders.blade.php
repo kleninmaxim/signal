2943a0bed42977562269eda4fd0bbb06f45a7568
= <maksim_klenin_99@mail.ru>
2022-01-14 12:27:42 +0300

    Change open orders style

--- resources/views/open-orders.blade.php
+++ resources/views/open-orders.blade.php
@@ -9,43 +9,7 @@
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
-    <style>
-        .table {
-            width: 100%;
-            border: none;
-            margin-bottom: 20px;
-        }
-        .table thead th {
-            font-weight: bold;
-            text-align: left;
-            border: none;
-            padding: 10px 15px;
-            background: #d8d8d8;
-            font-size: 14px;
-        }
-        .table thead tr th:first-child {
-            border-radius: 8px 0 0 8px;
-        }
-        .table thead tr th:last-child {
-            border-radius: 0 8px 8px 0;
-        }
-        .table tbody td {
-            text-align: left;
-            border: none;
-            padding: 10px 15px;
-            font-size: 14px;
-            vertical-align: top;
-        }
-        .table tbody tr:nth-child(even){
-            background: #f3f3f3;
-        }
-        .table tbody tr td:first-child {
-            border-radius: 8px 0 0 8px;
-        }
-        .table tbody tr td:last-child {
-            border-radius: 0 8px 8px 0;
-        }
-    </style>
+    <link rel="stylesheet" href="{{asset('css/style.css')}}" type="text/css" />
 
 </head>
 <body>
@@ -55,21 +19,21 @@
     <tr>
         <th>Symbol</th>
         <th>Size</th>
+        <th>PNL</th>
         <th>Entry Price</th>
         <th>Mark Price</th>
         <th>Liq. Price</th>
-        <th>PNL</th>
     </tr>
     </thead>
     <tbody>
     @forelse ($open_orders as $open_order)
         <tr>
             <td>{{$open_order['symbol']}}</td>
-            <td>{{$open_order['notional']}}</td>
+            <td>{{App\Hiney\Src\Math::round($open_order['notional'])}} USDT</td>
+            <td>{{App\Hiney\Src\Math::round($open_order['unRealizedProfit'])}} USDT</td>
             <td>{{$open_order['entryPrice']}}</td>
             <td>{{$open_order['markPrice']}}</td>
             <td>{{$open_order['liquidationPrice']}}</td>
-            <td>{{$open_order['unRealizedProfit']}}</td>
         </tr>
     @empty
 
