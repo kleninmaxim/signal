1bcf9486af34030874cfdc1e165f56f374e17337
= <maksim_klenin_99@mail.ru>
2022-01-23 19:00:34 +0300

    Add five percentage strategy with sell

--- app/Traits/Strategy/FivePercentageStrategy.php
+++ app/Traits/Strategy/FivePercentageStrategy.php
@@ -32,7 +32,8 @@ trait FivePercentageStrategy
                     ($candle['close'] - $price) / $price * 100
                 );
 
-            }
+            } else
+                $output[] = 0;
 
         }
 
@@ -41,6 +42,59 @@ trait FivePercentageStrategy
 /*        usort($output, function( $a, $b ) {
             return ($a['profit']-$b['profit']);
         });*/
+
+        return $output;
+
+    }
+
+    public static function FivePercentageWithSell($candles, $profit = 5)
+    {
+
+        $output = [];
+
+        foreach ($candles as $candle) {
+
+            $high = Math::percentage($candle['open'], $candle['high']);
+
+            $low = -1 * Math::percentage($candle['open'], $candle['low']);
+
+            if ($high >= $profit && $low >= $profit) {
+
+                $output[] = -2 * $profit;
+
+            } elseif ($high >= $profit) {
+
+                $price = $candle['open'] * (1 + $profit / 100);
+
+                /*                $output[] = [
+                                    'open' => $candle['open'],
+                                    'profit' => Math::round(
+                                        ($candle['close'] - $price) / $price * 100
+                                    ),
+                                    'date' => $candle['time_start'],
+                                ];*/
+
+                $output[] = Math::round(
+                    ($candle['close'] - $price) / $price * 100
+                );
+
+            } elseif ($low >= $profit) {
+
+                $price = $candle['open'] * (1 - $profit / 100);
+
+                $output[] = Math::round(
+                    ($price - $candle['close']) / $price * 100
+                );
+
+            }
+
+        }
+
+        //sort($output);
+
+        /*        usort($output, function( $a, $b ) {
+                    return ($a['profit']-$b['profit']);
+                });*/
 
         return $output;
 
